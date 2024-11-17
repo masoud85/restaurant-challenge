97940d5419bfb0272501f58057530d2bde289860
masoud.ahadi <masoud.ahadi@ocado.com>
2024-11-17 15:37:49 +0100

    Initial commit.

---
+++ src/test/java/com/midokura/restaurant/RestaurantChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midokura.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
